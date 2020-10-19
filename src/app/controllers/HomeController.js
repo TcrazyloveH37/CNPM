2d389ca032254b103ddc764524176f4a0e333453
DUCHUY <phamduchuy372000h@gmail.com>
2020-10-20 00:04:12 +0700

    test read products from database and then show

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,19 +1,11 @@
 const Product = require('../models/Product');
 
 class HomeController {
-  // [get], /home
-  index(req, res) {
-    res.render('home');
-  }
-
-  // [GET] /home/:slug
-  show(req, res) {
-    Product.find({}, (err, products) => {
-      if (!err) res.json(products);
-      else {
-        res.status(400).json({ error: 'message' });
-      }
-    });
+  // [get], /
+  index(req, res, next) {
+    Product.find({})
+      .then(products => res.render('home', { products }))
+      .catch(next);
   }
 }
 
