a704efc24ff789b055e90a4e8525ba5b13e7e724
ngoxuankien2000@gmail.com <ngoxuankien2000@gmail.com>
2020-11-29 21:15:52 +0700

    update signup

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,62 +1,198 @@
+// const form = document.querySelector("form");
+// const emailError = document.querySelector("form > .email");
+// const passwordError = document.querySelector("form > .password");
+// const nameError = document.querySelector("form > .name");
+// const addressError = document.querySelector("form > .address");
+// const phoneError = document.querySelector("form > .phone");
 
-const form = document.querySelector("form");
-const emailError = document.querySelector("form > .email");
-const passwordError = document.querySelector("form > .password");
-const nameError = document.querySelector("form > .name");
-const addressError = document.querySelector("form > .address");
-const phoneError = document.querySelector("form > .phone");
+// form.addEventListener("submit", async (e) => {
+//     e.preventDefault();
 
-form.addEventListener("submit", async (e) => {
-    e.preventDefault();
+//     // reset errors
+//     emailError.textContent = '';
+//     passwordError.textContent = '';
+//     nameError.textContent = '';
+//     addressError.textContent = '';
+//     phoneError.textContent = '';
 
-    // reset errors
-    emailError.textContent = '';
-    passwordError.textContent = '';
-    nameError.textContent = '';
-    addressError.textContent = '';
-    phoneError.textContent = '';
+//     //get the values;
+//     const email = form.email.value;
+//     const password = form.password.value;
+//     const name = form.name.value;
+//     const address = form.address.value;
+//     const phone = form.phone.value;
 
-    //get the values;
-    const email = form.email.value;
-    const password = form.password.value;
-    const name = form.name.value;
-    const address = form.address.value;
-    const phone = form.phone.value;
-    
-    let value;
-    const radios = document.getElementsByName('gender');
-    for (let i = 0, length = radios.length; i < length; i++) {
-        if (radios[i].checked) {
-            // do whatever you want with the checked radio
-            value = radios[i].value;
-            // only one radio can be logically checked, don't check the rest
-            break;
-        }
+//     let value;
+//     const radios = document.getElementsByName('gender');
+//     for (let i = 0, length = radios.length; i < length; i++) {
+//         if (radios[i].checked) {
+//             // do whatever you want with the checked radio
+//             value = radios[i].value;
+//             // only one radio can be logically checked, don't check the rest
+//             break;
+//         }
+//     }
+//     const gender = value;
+
+//     try {
+//         const res = await fetch("/sign-up", {
+//             method: "POST",
+//             body: JSON.stringify({ email, password, name, address, phone, gender }),
+//             headers: { "Content-Type": "application/json" },
+//         });
+
+//         const data = await res.json();
+
+//         if (data.errors) {
+//             emailError.textContent = data.errors.email;
+//             passwordError.textContent = data.errors.password;
+//             nameError.textContent = data.errors.name;
+//             addressError.textContent = data.errors.address;
+//             phoneError.textContent = data.errors.phone;
+//             genderError.textContent = data.errors.gender;
+//         }
+//         if (data.user) {
+//             location.assign('/');
+//         }
+//     } catch (err) {
+//         console.log(err);
+//     }
+// });
+
+function Validator(options) {
+  let selectorRules = {};
+
+  function validate(inputElement, rule) {
+    let errorElement = inputElement.parentElement.querySelector(
+      options.errorSelector,
+    );
+    let errorMessage;
+
+    let rules = selectorRules[rule.selector];
+
+    for (let i = 0; i < rules.length; ++i) {
+      errorMessage = rules[i](inputElement.value);
+      if (errorMessage) break;
     }
-    const gender = value;
 
-
-    try {
-        const res = await fetch("/sign-up", {
-            method: "POST",
-            body: JSON.stringify({ email, password, name, address, phone, gender }),
-            headers: { "Content-Type": "application/json" },
-        });
-
-        const data = await res.json();
-
-        if (data.errors) {
-            emailError.textContent = data.errors.email;
-            passwordError.textContent = data.errors.password;
-            nameError.textContent = data.errors.name;
-            addressError.textContent = data.errors.address;
-            phoneError.textContent = data.errors.phone;
-            genderError.textContent = data.errors.gender;
-        }
-        if (data.user) {
-            location.assign('/');
-        }
-    } catch (err) {
-        console.log(err);
+    if (errorMessage) {
+      errorElement.innerText = errorMessage;
+      inputElement.parentElement.classList.add('invalid');
+    } else {
+      let errorElement = inputElement.parentElement.querySelector(
+        options.errorSelector,
+      );
+      errorElement.innerText = '';
+      inputElement.parentElement.classList.remove('invalid');
     }
+  }
+
+  let formElement = document.querySelector(options.form);
+
+  if (formElement) {
+    // Khi submit form
+    formElement.onsubmit = function (e) {
+      e.preventDefault();
+
+      options.rules.forEach(function (rule) {
+        let inputElement = formElement.querySelector(rule.selector);
+        validate(inputElement, rule);
+      });
+    };
+
+    //Lap qua tung rule va xu ly su kien: blur, input,...
+    options.rules.forEach(function (rule) {
+      if (Array.isArray(selectorRules[rule.selector])) {
+        selectorRules[rule.selector].push(rule.test);
+      } else {
+        selectorRules[rule.selector] = [rule.test];
+      }
+
+      let inputElement = formElement.querySelector(rule.selector);
+      let errorElement = inputElement.parentElement.querySelector(
+        options.errorSelector,
+      );
+
+      if (inputElement) {
+        inputElement.onblur = function () {
+          validate(inputElement, rule);
+        };
+
+        inputElement.oninput = function () {
+          errorElement.innerText = '';
+          inputElement.parentElement.classList.remove('invalid');
+        };
+      }
+    });
+  }
+}
+
+//Dinh nghia rules
+Validator.isRequired = function (selector, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value.trim() ? undefined : message || 'Vui lòng nhập trường này';
+    },
+  };
+};
+
+Validator.isEmail = function (selector, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+      return regex.test(value)
+        ? undefined
+        : message || 'Trường này phải là email: ';
+    },
+  };
+};
+
+Validator.minLength = function (selector, min, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value.length >= min
+        ? undefined
+        : message || `Vui lòng nhập tối thiểu ${min} ký tự`;
+    },
+  };
+};
+
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value === getConfirmValue()
+        ? undefined
+        : message || 'Giá trị nhập vào không chính xác';
+    },
+  };
+};
+
+Validator({
+  form: '#form_sign_up',
+  errorSelector: '.form-message',
+  rules: [
+    Validator.isRequired('#fullname', 'Vui lòng nhập tên đầy đủ của bạn'),
+    Validator.isRequired('#email', 'Vui lòng nhập email của bạn'),
+    Validator.isEmail('#email', 'Đây không phải là email'),
+    Validator.minLength(
+      '#password',
+      6,
+      'Vui lòng nhập mật khẩu tối thiểu 6 ký tự',
+    ),
+    Validator.isRequired(
+      '#password_confirmation',
+      'Vui lòng nhập Nhập lại mật khẩu',
+    ),
+    Validator.isConfirmed(
+      '#password_confirmation',
+      function () {
+        return document.querySelector('#form_sign_up #password').value;
+      },
+      'Mật khẩu nhập lại không chính xác.',
+    ),
+  ],
 });
